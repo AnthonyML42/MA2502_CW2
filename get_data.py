e32cc7075e92763650844c4cbff24aef740d4b57
Anthony Lawrence <anthony@abc.com>
2025-03-20 14:18:44 +0000

    added python script to download kaggle data, only working on windows for now. Need to put raw data from LC on google drive

---
+++ get_data.py
@@ -0,0 +1,54 @@
+import requests
+import os
+import subprocess
+import gdown
+import sys
+import platform
+from pathlib import Path
+
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+
+if os.path.basename(os.path.normpath(dname)) != "MA2502_CW2":
+    sys.exit("Running outside of repo - wrong folder")
+
+Path("Data/zips").mkdir(parents=True, exist_ok=True)
+
+os.chdir(f"{dname}/Data/zips")
+
+files = [
+    ("historical-nba-data-and-player-box-scores.zip",
+     "https://www.kaggle.com/api/v1/datasets/download/eoinamoore/historical-nba-data-and-player-box-scores"),
+    ("nba-aba-baa-stats.zip",
+     "https://www.kaggle.com/api/v1/datasets/download/sumitrodatta/nba-aba-baa-stats"),
+    ("wyatt_basketball.zip",
+     "https://www.kaggle.com/api/v1/datasets/download/wyattowalsh/basketball")
+]
+
+running_os = platform.system()
+
+for filename, url in files:
+    print(f"Downloading {filename}...")
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    with open(filename, "wb") as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
+    print(f"Downloaded {filename}")
+
+    print(f"Extracting {filename}")
+    
+    if running_os == "Windows":
+        subprocess.run(["powershell", "-Command", f"Expand-Archive -Path {filename} -DestinationPath ../{os.path.splitext(filename)[0]}"])
+    elif running_os == "Linux":
+        pass
+    else:
+        raise NotImplementedError
+    
+    print(f"Extracted {filename}")
+
+
+os.chdir(f"{dname}")
+
